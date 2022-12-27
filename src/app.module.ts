4055f5f28be0f36227cd9a33faaaff791dfd763b
Aderlis  Díaz Rojas <Aderlis.Diaz@mheducation.com>
2022-12-27 14:15:26 -0400

    Added modules, controllers and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AirlinesModule } from './airlines/airlines.module';
+import { AirportsModule } from './airports/airports.module';
+import { FlightsModule } from './flights/flights.module';
 
 @Module({
-  imports: [],
+  imports: [AirlinesModule, AirportsModule, FlightsModule],
   controllers: [AppController],
   providers: [AppService],
 })
