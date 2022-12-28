4b18a0bd1542e17d2fa79fa46f079702a908d85d
Aderlis  Díaz Rojas <Aderlis.Diaz@mheducation.com>
2022-12-28 14:40:49 -0400

    Added Swagger and deleted unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,6 @@ import { APP_PIPE } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AirlinesModule } from './airlines/airlines.module';
 import { AirportsModule } from './airports/airports.module';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { configService } from './config/config.service';
 import { FlightsModule } from './flights/flights.module';
 
@@ -15,9 +13,7 @@ import { FlightsModule } from './flights/flights.module';
     AirportsModule,
     FlightsModule,
   ],
-  controllers: [AppController],
   providers: [
-    AppService,
     {
       provide: APP_PIPE,
       useValue: new ValidationPipe({ whitelist: true }),
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { configService } from './config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  if (!configService.isProduction()) {
+    const document = SwaggerModule.createDocument(
+      app,
+      new DocumentBuilder()
+        .setTitle('Airports, Airlines and Flights API')
+        .setDescription(
+          'Application to handle CRUD for Airports, Airlines and Flights',
+        )
+        .build(),
+    );
+
+    SwaggerModule.setup('swagger', app, document);
+  }
+
   await app.listen(3000);
 }
 bootstrap();
